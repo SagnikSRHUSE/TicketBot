c1b8a9f9a33113774400cb58c864bc19ec513f14
Zendovo <33803331+Zendovo@users.noreply.github.com>
2019-03-15 13:56:09 +0530

    Update closeticket.js

--- commands/closeticket.js
+++ commands/closeticket.js
@@ -7,57 +7,39 @@ module.exports.run = async (bot, message, args, con, prefix, tcMessage, staffrol
     var ticketlog = message.guild.channels.find("name", "ticket-log");
     let closedAt = message.createdAt;
 
-    con.query(`SELECT * FROM tickets WHERE ticketID = '${channelName}'`, (err, rows) => {
+    if(!message.member.roles.find("name", staffrole)) return message.channel.send("You do not have the permission to close a ticket. If you want one of your tickets to be closed please contact a staff.")
 
-        if(err) throw err;
+    if(rows.length < 1) {
+        message.channel.send("You are not in a ticket channel! Tickets can only be closed in the channel itself.");
 
-        if(!message.member.roles.find("name", staffrole)) return message.channel.send("You do not have the permission to close a ticket. If you want one of your tickets to be closed please contact a staff.")
+    } else {
 
-        if(rows.length < 1) {
-            message.channel.send("You are not in a ticket channel! Tickets can only be closed in the channel itself.");
+        if(!args[0]){    
+            channel.delete();
+            var closeTicket = new Discord.RichEmbed()
+              .setDescription("**Ticket Closed**")
+              .setColor("#ed3434")
+              .addField("Ticket Closed by:", `${message.author}`, true)
+              .addField("Closed At:", `${closedAt}`, true)
+              .addField("Ticket-ID:", `${channelName}`, true)
+              .addField("Closing Reason:", `Not defined`, true);
+            ticketlog.send(closeTicket);
 
-        } else {
-
-            if(!args[0]){
-
-                let changeStatus;
-                changeStatus = `UPDATE tickets SET status = 'closed' WHERE ticketID = '${channelName}'`;
-                con.query(changeStatus);
-    
-                channel.delete();
-    
-                var closeTicket = new Discord.RichEmbed()
-                  .setDescription("**Ticket Closed**")
-                  .setColor("#ed3434")
-                  .addField("Ticket Closed by:", `${message.author}`, true)
-                  .addField("Closed At:", `${closedAt}`, true)
-                  .addField("Ticket-ID:", `${channelName}`, true);
-                ticketlog.send(closeTicket);
-
-            } else {
-
-                let changeStatus;
-                changeStatus = `UPDATE tickets SET status = 'closed' WHERE ticketID = '${channelName}'`;
-                con.query(changeStatus);
-    
-                channel.delete();
-
-                let closeReason = args.join(" ");
-                if (closeReason.contains("'")) closeReason.replace("'", "");
-                con.query(`UPDATE tickets SET closeReason = '${closeReason}' WHERE ticketID = '${channelName}'`);
-    
-                var closeTicket = new Discord.RichEmbed()
-                  .setDescription("**Ticket Closed**")
-                  .setColor("#ed3434")
-                  .addField("Ticket Closed by:", `${message.author}`, true)
-                  .addField("Closed At:", `${closedAt}`, true)
-                  .addField("Ticket-ID:", `${channelName}`, true);
-                ticketlog.send(closeTicket);
-
-            }
+        } else {    
+            channel.delete();
+            let closeReason = args.join(" ");
+            var closeTicket = new Discord.RichEmbed()
+              .setDescription("**Ticket Closed**")
+              .setColor("#ed3434")
+              .addField("Ticket Closed by:", `${message.author}`, true)
+              .addField("Closed At:", `${closedAt}`, true)
+              .addField("Ticket-ID:", `${channelName}`, true)
+              .addField("Closing Reason:", `${closeReason}`, true);
+            ticketlog.send(closeTicket);
 
         }
-    });
+
+    }
 
 }
 
