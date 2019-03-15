442ec68182dcc2a6ddc091557081be9828a8e1ea
Zendovo <33803331+Zendovo@users.noreply.github.com>
2019-03-15 13:57:37 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports.run = async (bot, message, args, con, prefix) => {
         let help = new Discord.RichEmbed()
         .setDescription(`**Help Menu**\nDo ${prefix}help [type]`)
         .addField("Tickets:", "Info for tickets.")
-        .addField("Settings:", "Info about settings.")
         .setFooter("<> = Required | [] = optional")
         .setColor("#3def15");
         message.channel.send(help);
@@ -19,21 +18,9 @@ module.exports.run = async (bot, message, args, con, prefix) => {
         .addField(`${prefix}newticket [reason]`, "Create a ticket. You can optionally provide a reason.")
         .addField(`${prefix}ticketfor <@user> [reason]`, "Staff Only! Creates a ticket for the mentioned user.")
         .addField(`${prefix}closeticket [reason]`, "Staff Only! Closes the ticket. Use it in the ticket channel itself.")
-        .addField(`${prefix}ticketinfo <ticket-ID or ID>`, "Staff Only! Shows information about a ticket.")
         .setFooter("<> = Required | [] = optional")
         .setColor("#3def15");
         message.channel.send(ticketHelp);
-
-    } else if(args[0] === "settings" || args[0] === "setting"){
-
-        let settingHelp = new Discord.RichEmbed()
-        .setDescription(`**Settings**\nList of all Commands\n**ADMIN ONLY**`)
-        .addField(`${prefix}settings list`, "Lists all settings which you can change.")
-        .addField(`${prefix}settings info <type>`, "Sends the value of the setting.")
-        .addField(`${prefix}settings set <type>`, "Sets a new value for the setting.")
-        .setFooter("<> = Required | [] = optional")
-        .setColor("#3def15");
-        message.channel.send(settingHelp);
 
     } else {
         message.channel.send("Not a valid type. Do `" + prefix + "help` to see the valid types.");
