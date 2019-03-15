41c1d6fd8bcca6884408fd067aa0960c2f82cfcc
Zendovo <zendovo@debian>
2019-03-15 14:40:19 +0530

    forgot to add declaration fr randomstring

--- commands/ticketfor.js
+++ commands/ticketfor.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+var randomstring = require("randomstring");
 
 module.exports.run = async (bot, message, args, prefix, tcMessage, staffrole, adminrole) => {
 
