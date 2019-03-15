6825ab0c619f7007a1cc742fe32b0fd80060fdd1
Zendovo <zendovo@debian>
2019-03-15 14:14:22 +0530

    Fixed bugs

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const fs = require("fs");
 
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
