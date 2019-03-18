9c2da5f27694e0e6217635fce29fc65e79cea5ed
Zendovo <zendovo@debian>
2019-03-18 19:15:23 +0530

    Fixed restart and shutdown

--- commands/restart.js
+++ commands/restart.js
@@ -1,20 +1,27 @@
 const Discord = require("discord.js");
-const botconfig = require("./botconfig.json");
+const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
-    if(!message.author.id == "109883996019658752" || !message.author.id == "142416518888554497" || !message.author.id == "231978239063490560") return;
+    if(message.author.id !== "109883996019658752" && message.author.id !== "142416518888554497" && message.author.id !== "231978239063490560") return console.log(message.author.id + " tried to restart!");
 
-    bot.destroy();
-    if (!args[0]) return bot.login(botconfig.token);
-    message.channel.send("Restarting!");
+    function login() {
+        bot.login(botconfig.token);
+        message.channel.send(":white_check_mark: Restarted!");
+    }
+
+    message.channel.send(":arrows_counterclockwise: Restarting!");
+    
+    if (!args[0]) return bot.destroy().then((login));
 
     if (args[0]){
+        bot.destroy();
+
         let timeInS = parseInt(args[0], 10) || 0;
         if (timeInS == 0) return message.channel.send(":stop_sign: Enter a valid Integer! (in seconds)");
         let timeInMS = timeInS * 1000; 
 
-        setTimeout(bot.login(botconfig.token), timeInMS);
+        setTimeout(login, timeInMS);
     }
 
 }
--- commands/shutdown.js
+++ commands/shutdown.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
-    if(!message.author.id == "109883996019658752" || !message.author.id == "142416518888554497" || !message.author.id == "231978239063490561") return;
+    if(message.author.id !== "109883996019658752" && message.author.id !== "142416518888554497" && message.author.id !== "231978239063490560") return console.log(message.author.id + " tried to shutdown!");
 
-    bot.destroy();
+    message.channel.send(":stop_button: Shutting Down!").then(bot.destroy());
 
 }
 
